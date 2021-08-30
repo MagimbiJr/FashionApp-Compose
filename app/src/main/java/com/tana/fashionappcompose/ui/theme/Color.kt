4857f849bb33c7b52cc50f969148e0ce28026c90
atanas <charleatanasi@gmail.com>
2021-08-30 13:20:27 +0300

    Initial Commit

---
+++ app/src/main/java/com/tana/fashionappcompose/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.tana.fashionappcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val White = Color(0xFFffffff)
+val DarkBlue = Color(0xFF100B15)
+val LightPurple = Color(0xFFb988ea)
+val Gray = Color(0xFF9A9A9E)
+
+val testColor = Color(0xFF03DAC6)
